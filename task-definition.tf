9d681c8e9063d5649726da8c7174b687f02d65f0
Caleb Black <calebmacdonaldblack@gmail.com>
2020-11-08 09:00:12 +1000

    Create a more generic fargate module

---
+++ task-definition.tf
@@ -0,0 +1,111 @@
+#------------------------------------------------------------------------------
+# AWS ECS Task Execution Role
+#------------------------------------------------------------------------------
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name               = "${var.name_prefix}-ecs-execution-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy" "ecs_execution_role_policy" {
+  name   = "${var.name_prefix}-role-policy"
+  role   = aws_iam_role.ecs_execution_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role" "task" {
+  name               = "${var.name_prefix}-task-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy_attachement" {
+  policy_arn = var.task_policy_arn
+  role       = aws_iam_role.ecs_execution_role.name
+}
+
+resource "aws_ecs_task_definition" "service" {
+  family                   = var.name_prefix
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.container_cpu
+  memory                   = var.container_memory
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.task.arn
+  container_definitions    = <<EOF
+[
+  {
+    "name": "${var.name_prefix}",
+    "image": "${var.container_image}",
+    "portMappings": [
+      {
+        "containerPort": 5000,
+        "hostPort": 5000
+      }
+    ],
+    "environment": ${jsonencode(concat([{name = "AWS_CONTAINER_CREDENTIALS_RELATIVE_URI", value = "creds"}]var.container_environment_variables))}
+    ,
+    "cpu": ${var.container_cpu},
+    "memory": ${var.container_memory},
+    "networkMode": "awsvpc",
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "${var.log_group_name}",
+        "awslogs-region": "${var.log_group_region}",
+        "awslogs-stream-prefix": "${var.name_prefix}"
+      }
+    }
+  }
+]
+EOF
+
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,49 @@ variable "name_prefix" {
   default = "The name prefix of the resources created in this module."
   type = string
 }
+
+variable "name_prefix" {
+  description = "The name prefix of the resources created in this module."
+  type        = string
+}
+
+variable "container_cpu" {
+  description = "The amount of cpu to allocate to the task"
+  type        = number
+  default     = 256
+}
+
+variable "container_memory" {
+  description = "The amount of memory to allocate to the task"
+  type        = number
+  default     = 512
+}
+
+variable "container_image" {
+  description = "The docker image to run for the task"
+  type        = string
+}
+
+variable "container_environment_variables" {
+  description = "The environment variables for the container"
+  type        = list(object({
+    name  = string
+    value = string
+  }))
+  default     = []
+}
+
+variable "log_group_name" {
+  description = "The name of the log group for the container"
+  type        = string
+}
+
+variable "log_group_region" {
+  description = "The name of the log group for the container"
+  type        = string
+}
+
+variable "task_policy_arn" {
+  description = "The arn of the task policy"
+  type        = string
+}
