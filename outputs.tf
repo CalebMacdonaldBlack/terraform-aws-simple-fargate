b6efa9f708b0cb6babeadf6c11cf26f83374b37d
Caleb Black <calebmacdonaldblack@gmail.com>
2020-11-08 10:04:16 +1000

    Add alb outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "ecs_service_name" {
   value = aws_ecs_service.service.name
 }
+
+output "alb_dns_name" {
+  value = aws_lb.lb.dns_name
+}
+
+output "alb_zone_id" {
+  value = aws_lb.lb.zone_id
+}
